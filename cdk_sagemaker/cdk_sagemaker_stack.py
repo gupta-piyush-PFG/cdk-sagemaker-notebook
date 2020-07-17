cd3fc18148db8ab2aa1c889aea06555aadb9292f
piyush9797-PFG <54705884+piyush9797-PFG@users.noreply.github.com>
2020-07-17 20:20:59 +0530

    Update cdk_sagemaker_stack.py

--- cdk_sagemaker/cdk_sagemaker_stack.py
+++ cdk_sagemaker/cdk_sagemaker_stack.py
@@ -17,7 +17,7 @@ class CdkSagemakerStack(core.Stack):
         bucket2 = s3.Bucket(self,
     		"BucketCDK1", 
     		versioned=True,
-    		bucket_name='minotaur-data-bucket-1',
+    		bucket_name='minotaur-project-files-bucket',
             removal_policy=core.RemovalPolicy.DESTROY)
 
         sm_notebook = sagemaker.CfnNotebookInstance(self,
