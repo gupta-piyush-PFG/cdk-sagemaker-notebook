1cedb519160f34ffc920b6baf6e8cc86e350c072
piyush9797-PFG <54705884+piyush9797-PFG@users.noreply.github.com>
2020-07-17 17:49:49 +0530

    Update cdk_sagemaker_stack.py

--- cdk_sagemaker/cdk_sagemaker_stack.py
+++ cdk_sagemaker/cdk_sagemaker_stack.py
@@ -11,18 +11,18 @@ class CdkSagemakerStack(core.Stack):
         bucket1 = s3.Bucket(self,
     		"BucketCDK", 
     		versioned=True,
-    		bucket_name='Minotaur_data_bucket',
+    		bucket_name='minotaur_data_bucket',
             removal_policy=core.RemovalPolicy.DESTROY)
         
         bucket2 = s3.Bucket(self,
     		"BucketCDK", 
     		versioned=True,
-    		bucket_name='Minotaur_project_files_bucket',
+    		bucket_name='minotaur_project_files_bucket',
             removal_policy=core.RemovalPolicy.DESTROY)
 
         sm_notebook = sagemaker.CfnNotebookInstance(self,
         "SageMakerNotebookInstance",
          instance_type='ml.m4.xlarge',
          role_arn='arn:aws:iam::304472691870:role/aws-sagemaker-role-s843971',
-         notebook_instance_name='Minotaur_notebook',
+         notebook_instance_name='minotaur_notebook',
          default_code_repository='https://github.com/abdul-pfg/sagemaker-iris')
